7fe5c9d0750a6b677e6edf28676b365143792110
Shubham Talan <shubhamsinghtalan08@gmail.com>
2024-01-14 23:59:47 +0530

    feat(validation): added express validator

--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { body } = require("express-validator");
+
 const feedController = require("../controller/feed");
 
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
 
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title", "Please enter a valid Title!").trim().isLength({ min: 7 }),
+    body("content", "Please enter a valid description.")
+      .trim()
+      .isLength({ min: 8, max: 400 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
